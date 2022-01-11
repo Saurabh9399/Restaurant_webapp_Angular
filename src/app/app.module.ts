2202d151ce1ed96b8391124939704cf40c6f1c92
saurabh <saurabhj8087445@gmail.com>
2022-01-11 15:34:58 +0530

    add project files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestaurantDashComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-restaurant-dash',
+  templateUrl: './restaurant-dash.component.html',
+  styleUrls: ['./restaurant-dash.component.css']
+})
+export class RestaurantDashComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
